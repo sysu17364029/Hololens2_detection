1337f10eb25fa602adc2ca49b2c819c0d6164772
sysu17364029 <57244715+sysu17364029@users.noreply.github.com>
2021-04-16 23:35:18 +0800

    Create StartReceive.cs

--- UnityHL2RmStreamer/Assets/Scripts/StartReceive.cs
+++ UnityHL2RmStreamer/Assets/Scripts/StartReceive.cs
@@ -17,39 +17,18 @@ public class StartReceive : MonoBehaviour
 {
     public Text myText;
     private string message;
-    //private Socket client;
     private Socket server;
     private Socket worker;
-    //private string host = "192.168.43.123";
     private string host = "127.0.0.1";
     //private string host = "0.0.0.0";
     private int port = 5000;
     private byte[] messTmp;
-    private GameObject LineRenderGameObject;
-    private LineRenderer lineRenderer;
-    private int lineLength = 5;
     private List<string> DataList = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
-        //Thread.Sleep(5000);
         messTmp = new byte[1024];
-        //client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        //try
-        //{
-        //    client.Connect(new IPEndPoint(IPAddress.Parse(host), port));
-        //}
-        //catch (Exception e)
-        //{
-        //    Console.WriteLine(e.Message);
-        //    return;
-        //}
-
-        LineRenderGameObject = GameObject.Find("line");
-        lineRenderer = (LineRenderer)LineRenderGameObject.GetComponent("LineRenderer");
-        //lineRenderer.SetVertexCount(lineLength);
-        lineRenderer.positionCount = lineLength;
 
         server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         server.Bind(new IPEndPoint(IPAddress.Parse(host), port));
@@ -90,20 +69,6 @@ public class StartReceive : MonoBehaviour
         DataList.Add(data);
     }
 
-    //void GetMessage()
-    //{
-    //    var count = client.Receive(messTmp);
-
-    //    if (count != 0)
-    //    {
-    //        Debug.Log(count);
-    //        string message = Encoding.UTF8.GetString(messTmp, 0, count);
-    //        Debug.Log(message);
-    //        //myText.GetComponent<Text>().text = message;
-    //        ParseItemJson(message);
-    //    }
-    //}
-
     void ParseItemJson(string jsonStr)
     {
         jsonStr = "{\"list\":" + jsonStr + "}";
@@ -121,15 +86,6 @@ public class StartReceive : MonoBehaviour
             GameObject gameObject = GameObject.Find("Main Camera"); 
             TextToSpeech textToSpeech = gameObject.GetComponent<TextToSpeech>();
             textToSpeech.StartSpeaking(item.Name);
-            Vector3 v0 = new Vector3((float)item.X1, (float)item.Y1, 0.0f);
-            Vector3 v1 = new Vector3((float)item.X2, (float)item.Y1, 0.0f);
-            Vector3 v2 = new Vector3((float)item.X2, (float)item.Y2, 0.0f);
-            Vector3 v3 = new Vector3((float)item.X1, (float)item.Y2, 0.0f);
-            lineRenderer.SetPosition(0, v0);
-            lineRenderer.SetPosition(1, v1);
-            lineRenderer.SetPosition(2, v2);
-            lineRenderer.SetPosition(3, v3);
-            lineRenderer.SetPosition(4, v0);
         }
     }
     public class Response<T>
@@ -153,11 +109,17 @@ public class StartReceive : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //GetMessage();
         if (DataList.Count > 0) 
         {
-            ParseItemJson(DataList[0]);
-            DataList.RemoveAt(0);
+            try
+            {
+                ParseItemJson(DataList[0]);
+            }
+            catch {}
+            finally
+            {
+                DataList.RemoveAt(0);
+            }
         }
     }
 }
